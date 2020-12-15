3ef5e48626f3895ded47ddd7df8ed200c4d22984
Tiago Miranda <tiago.e.miranda@gmail.com>
2020-12-15 15:06:35 +0000

    CRUD de produtos para a web

--- LojaCet49/Dados/Entidades/Produto.cs
+++ LojaCet49/Dados/Entidades/Produto.cs
@@ -10,6 +10,7 @@ namespace LojaCet49.Dados.Entidades
 
         public string Nome { get; set; }
 
+        [Display(Name = "Preço")]
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Preco { get; set; }
 
